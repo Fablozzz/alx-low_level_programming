8b8ef8773b7e91088119c6b2dc3b241fa8c6573d
Fablozzz <jstfablozzz@gmail.com>
2022-09-16 11:29:44 -0700

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,7 +11,7 @@
 int largest_number(int a, int b, int c)
 {
 int largest;
-if (a >= b && b >= c)
+if (a >= b && a >= c)
 {
 largest = a;
 }
@@ -23,6 +23,5 @@ else
 {
 largest = c;
 }
-
 return (largest);
 }
